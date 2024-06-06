7eccf88b9c22445d87261d4839d6917bf9bb1246
Thomas Ashraf <66570810+thomas-x-69@users.noreply.github.com>
2024-06-06 17:59:31 +0300

    Time: 86 ms (93.47%) | Memory: 60.6 MB (40.41%) - LeetSync

---
+++ 876-hand-of-straights/hand-of-straights.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[]} hand
+ * @param {number} groupSize
+ * @return {boolean}
+ */
+var isNStraightHand = function(hand, groupSize) {
+    if (hand.length % groupSize != 0) {
+    return false;
+    }
+    let countMap = new Map();
+
+  for (let card of hand) {
+    countMap.set(card, (countMap.get(card) || 0) + 1);
+  }
+
+  hand.sort((a, b) => a - b);
+
+    for (let card of hand) {
+    if (countMap.get(card) == 0) {
+      continue;
+    }
+
+    for (i = 0; i < groupSize; i++) {
+    let currCard = card + i;
+
+      if (countMap.get(currCard) == undefined || countMap.get(currCard) == 0) {
+        return false;
+      }
+
+      countMap.set(currCard, countMap.get(currCard) - 1);
+    }}
+    return true
+};
