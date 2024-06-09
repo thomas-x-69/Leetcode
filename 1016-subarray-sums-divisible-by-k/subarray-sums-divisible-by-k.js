3c55ccedf1146f46932687153d7ad682789db165
Thomas Ashraf <66570810+thomas-x-69@users.noreply.github.com>
2024-06-09 11:27:05 +0300

    Time: 93 ms (15.31%) | Memory: 54.4 MB (59.18%) - LeetSync

---
+++ 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var subarraysDivByK = function(nums, k) {
+   	let count = 0, sum = 0;
+	
+	let freq = new Array(k).fill(0);
+	freq[0] = 1;
+	
+	for (let n of nums) {
+		sum = (sum + n) % k;
+	   
+		if (sum < 0)
+			sum += k;
+		
+		count += freq[sum];
+		freq[sum]++;
+	}
+	
+	return count;
+
+};
