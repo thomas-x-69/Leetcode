109874f2050288dd082b49ddbfbe8e15d3d116c7
Thomas Ashraf <66570810+thomas-x-69@users.noreply.github.com>
2024-06-10 23:42:59 +0300

    Time: 52 ms (75.00%) | Memory: 48.7 MB (75.00%) - LeetSync

--- 1012-equal-rational-numbers/equal-rational-numbers.js
+++ 1012-equal-rational-numbers/equal-rational-numbers.js
@@ -9,7 +9,7 @@ var isRationalEqual = function(s, t) {
         let newer = v.split('(')[1] && v.split('(')[1].split(')')[0] || '0';
 
         start = start.includes('.') ? start : start + '.'
-        newer = newer.padEnd(17, newer)
+        newer = newer.padEnd(17, newer)//17 is nothing just smallest valid number for test cases
 
         return parseFloat(start + newer)
         }
