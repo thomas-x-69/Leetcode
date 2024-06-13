160c1da2d2076aa6b2859799b6997506464af1f4
Thomas Ashraf <66570810+thomas-x-69@users.noreply.github.com>
2024-06-13 20:22:09 +0300

    Time: 48 ms (73.98%) | Memory: 48.8 MB (46.75%) - LeetSync

--- 468-validate-ip-address/validate-ip-address.js
+++ 468-validate-ip-address/validate-ip-address.js
@@ -2,4 +2,8 @@
  * @param {string} queryIP
  * @return {string}
  */
-var validIPAddress= (IP)=> (/^((\d|[1-9]\d|1\d\d|2([0-4]\d|5[0-5]))\.){4}$/).test(IP + '.') ? 'IPv4':(/^([\da-f]{1,4}:){8}$/i).test(IP + ':') ? 'IPv6' : 'Neither';
+var validIPAddress = function(IP) {
+    const ipv4 = /^((\d|[1-9]\d|1\d\d|2([0-4]\d|5[0-5]))\.){4}$/;
+    const ipv6 = /^([\da-f]{1,4}:){8}$/i;
+    return ipv4.test(IP + '.') ? 'IPv4' : ipv6.test(IP + ':') ? 'IPv6' : 'Neither';
+    };
