9ac356561769ffb0c7208fb799519764a24f9dac
Thomas Ashraf <66570810+thomas-x-69@users.noreply.github.com>
2024-06-13 03:36:08 +0300

    Time: 62 ms (37.56%) | Memory: 50.4 MB (90.36%) - LeetSync

---
+++ 2148-minimum-number-of-moves-to-seat-everyone/minimum-number-of-moves-to-seat-everyone.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} seats
+ * @param {number[]} students
+ * @return {number}
+ */
+var minMovesToSeat = function(seats, students) {
+    seats = seats.sort((a,b)=>a-b)
+    students = students.sort((a,b)=>a-b)
+        let sum = 0
+
+    for(let i=0;i<seats.length;i++)
+        sum+=Math.abs(seats[i]-students[i])
+    return sum
+};
