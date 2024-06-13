dc37713073689a90c8bad8ba9803f393a89a45e5
Thomas Ashraf <66570810+thomas-x-69@users.noreply.github.com>
2024-06-13 20:21:22 +0300

    Time: 57 ms (26.42%) | Memory: 48.6 MB (82.52%) - LeetSync

--- 468-validate-ip-address/validate-ip-address.js
+++ 468-validate-ip-address/validate-ip-address.js
@@ -2,32 +2,8 @@
  * @param {string} queryIP
  * @return {string}
  */
-var validIPAddress = function(queryIP) {
-	const iPv4 = () => {
-		const address = queryIP.split('.');
-		if (address.length !== 4) return null;
+var validIPAddress = function(IP) {
 
-		for (const str of address) {
-			const ip = parseInt(str);
-			if (ip < 0 || ip > 255) return null;
-			if (ip.toString() != str) return null;
-		}
-		return 'IPv4';
-	};
-
-	const iPv6 = () => {
-		const address = queryIP.split(':');
-		if (address.length !== 8) return null;
-		const config = '0123456789abcdefABCDEF';
-		const check = address.every(str => {
-			if (str === '' || str.length > 4) return false;
-			for (const s of str) {
-				if (!config.includes(s)) return false;
-			}
-			return true;
-		});
-		return check ? 'IPv6' : null; 
-	};
-
-	return iPv4() ?? iPv6() ?? 'Neither';
+    return (/^((\d|[1-9]\d|1\d\d|2([0-4]\d|5[0-5]))\.){4}$/).test(IP + '.') 
+    ? 'IPv4' : (/^([\da-f]{1,4}:){8}$/i).test(IP + ':') ? 'IPv6' : 'Neither';
     };
