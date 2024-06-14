59df26bcc515a4ab1192d4c82024c4c24af2e19b
Thomas Ashraf <66570810+thomas-x-69@users.noreply.github.com>
2024-06-15 00:18:12 +0300

    Time: 58 ms (76.19%) | Memory: 56.2 MB (47.62%) - LeetSync

---
+++ 929-groups-of-special-equivalent-strings/groups-of-special-equivalent-strings.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string[]} words
+ * @return {number}
+ */
+var numSpecialEquivGroups = function(words) {
+    let set = new Set();
+    words.forEach(a => set.add(transform(a)));
+    return set.size;
+};
+
+function transform(s){
+    let even = s.split('').filter((_, i) => i % 2 === 0);
+    let odd = s.split('').filter((_, i) => i % 2 === 1);
+    even.sort();
+    odd.sort();
+    return `${even.join('')}${odd.join('')}`;
+
+};
