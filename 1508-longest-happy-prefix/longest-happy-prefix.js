77b9df6abcd5833542e1a40a22f22aa42dacd880
Thomas Ashraf <66570810+thomas-x-69@users.noreply.github.com>
2024-06-15 00:45:06 +0300

    Time: 253 ms (51.43%) | Memory: 56.7 MB (91.43%) - LeetSync

---
+++ 1508-longest-happy-prefix/longest-happy-prefix.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var longestPrefix = function(s) {
+    for (let i = 1; i < s.length; i++) {
+    let a = s.slice(i);
+    let b = s.slice(0, s.length - i);
+    if (a == b) {
+      return a;
+    }
+  }
+
+  return "";
+};
