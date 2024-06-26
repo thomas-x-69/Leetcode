b560bbb7072d420a022c304670b51ce035860bd9
Thomas Ashraf <66570810+thomas-x-69@users.noreply.github.com>
2024-06-26 04:50:23 +0300

    Time: 566 ms (82.22%) | Memory: 0B (100.00%) - LeetSync

--- 1182-game-play-analysis-iv/game-play-analysis-iv.sql
+++ 1182-game-play-analysis-iv/game-play-analysis-iv.sql
@@ -1,5 +1,7 @@
 # Write your MySQL query statement below
-SELECT ROUND(COUNT(DISTINCT player_id) / (SELECT COUNT(DISTINCT player_id) FROM Activity), 2) as fraction
-FROM Activity
-WHERE (player_id, DATE_SUB(event_date, INTERVAL 1 DAY))
-IN (SELECT player_id, MIN(event_date) AS first_login FROM ACTIVITY GROUP BY player_id)
+select round(count(distinct player_id)/(select count(distinct player_id) from activity), 2) as fraction
+from activity
+where (player_id, date_sub(event_date, interval 1 day)) 
+in (select player_id, min(event_date) as first_date
+from activity
+group by player_id)
