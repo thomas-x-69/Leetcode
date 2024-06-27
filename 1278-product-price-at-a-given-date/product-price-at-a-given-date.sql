598568ace17408729f68959fe8b34d9b3ee9b5e9
Thomas Ashraf <66570810+thomas-x-69@users.noreply.github.com>
2024-06-27 04:45:16 +0300

    Time: 480 ms (78.46%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
@@ -0,0 +1,27 @@
+# Write your MySQL query statement below
+select 
+    distinct product_id,
+    10 as price
+from 
+    products
+group by
+    product_id
+    having
+    min(change_date) > "2019-08-16"
+union 
+select 
+    product_id, 
+    new_price
+from 
+    Products 
+where 
+(product_id, change_date) in(
+        select
+        product_id,
+        max(change_date)
+        from products
+        where
+         change_date <= "2019-08-16"
+            group by 
+            product_id
+    )
