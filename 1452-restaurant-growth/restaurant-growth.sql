905814a5241f6453359357918aa2940ac6ccee71
Thomas Ashraf <66570810+thomas-x-69@users.noreply.github.com>
2024-06-28 07:17:25 +0300

    Time: 343 ms (78.47%) | Memory: 0B (100.00%) - LeetSync

--- 1452-restaurant-growth/restaurant-growth.sql
+++ 1452-restaurant-growth/restaurant-growth.sql
@@ -1,8 +1,8 @@
 # Write your MySQL query statement below
 select visited_on,
-sum(sum(amount)) over (rows between 6 preceding and current row) as amount,
+sum(sum(amount)) OVER (rows between 6 preceding and current row) as amount,
 round(avg(sum(amount)) OVER (rows between 6 preceding and current row),2) AS average_amount
 from Customer
 group by visited_on
 order by visited_on 
-limit 999999 offset 6;
+limit 99999 offset 6;
