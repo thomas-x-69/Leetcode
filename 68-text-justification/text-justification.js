7cde840d9bca10f7962964edf27637727e5eaeab
Thomas Ashraf <66570810+thomas-x-69@users.noreply.github.com>
2024-08-11 19:13:44 +0300

    Time: 45 ms (86.63%) | Memory: 49 MB (28.66%) - LeetSync

--- 68-text-justification/text-justification.js
+++ 68-text-justification/text-justification.js
@@ -4,26 +4,39 @@
  * @return {string[]}
  */
 var fullJustify = function(words, maxWidth) {
-     let res = [];
-        let cur = [];
-        let num_of_letters = 0;
+        let res = []
+    let k = 0
+    let temp = []
+    let wordLength = 0
 
-        for (let word of words) {
-            if (word.length + cur.length + num_of_letters > maxWidth) {
-                for (let i = 0; i < maxWidth - num_of_letters; i++) {
-                    cur[i % (cur.length - 1 || 1)] += ' ';
+    for (let i = 0; i < words.length; i++) {
+        temp.push(words[i])
+        wordLength += words[i].length
+
+        if (wordLength + words[i + 1]?.length + temp.length > maxWidth) {
+            if (temp.length === 1) {
+                res[k] = temp[0] + ' '.repeat(maxWidth - wordLength)
+
+            } else {
+                let remainder = (maxWidth - wordLength) % (temp.length - 1)
+                let quotient = (maxWidth - wordLength - remainder) / (temp.length - 1)
+
+                res[k] = temp[0]
+                for (let j = 1; j < temp.length; j++) {
+                    let space = ' '.repeat(quotient)
+                    if (remainder > 0) {
+                        space += ' '
+                        remainder--
+                    }
+                    res[k] = `${res[k]}${space}${temp[j]}`
                 }
-                res.push(cur.join(''));
-                cur = [];
-                num_of_letters = 0;
             }
-            cur.push(word);
-            num_of_letters += word.length;
+            k++
+            wordLength = 0
+            temp = []
+        } else if (i === words.length - 1) {
+            res[k] = temp.join(' ') + ' '.repeat(maxWidth - wordLength - temp.length + 1)
         }
-
-        let lastLine = cur.join(' ');
-        while (lastLine.length < maxWidth) lastLine += ' ';
-        res.push(lastLine);
-
-        return res;
+    }
+    return res
 };
