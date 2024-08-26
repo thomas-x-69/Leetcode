25df79e845677bd3b278d12c8406857c50c22367
Thomas Ashraf <66570810+thomas-x-69@users.noreply.github.com>
2024-08-26 19:01:04 +0300

    Time: 46 ms (88.89%) | Memory: 49.3 MB (19.44%) - LeetSync

---
+++ 1103-moving-stones-until-consecutive/moving-stones-until-consecutive.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number} a
+ * @param {number} b
+ * @param {number} c
+ * @return {number[]}
+ */
+var numMovesStones = function(a, b, c) {
+    let stones = [a, b, c].sort((x, y) => x - y);
+
+    let dist1 = stones[1] - stones[0] - 1;
+    let dist2 = stones[2] - stones[1] - 1;
+
+    let min, max;
+    if (dist1 == 0 && dist2 == 0) {
+       min = 0;
+       max = 0;
+   } else if (dist1 == 0 || dist2 == 0 || dist1 == 1 || dist2 == 1) {
+       min = 1;
+       max = dist1 + dist2;
+   } else {
+       min = 2;
+       max = dist1 + dist2;
+   }
+
+   return [min, max];
+};
