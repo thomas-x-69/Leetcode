7b7f9b4707f0c1dc6c978a33f1b131596764bc18
Thomas Ashraf <66570810+thomas-x-69@users.noreply.github.com>
2024-10-12 14:24:10 +0300

    Time: 214 ms (95.00%) | Memory: 72.6 MB (100.00%) - LeetSync

--- 2488-divide-intervals-into-minimum-number-of-groups/divide-intervals-into-minimum-number-of-groups.js
+++ 2488-divide-intervals-into-minimum-number-of-groups/divide-intervals-into-minimum-number-of-groups.js
@@ -3,15 +3,21 @@
  * @return {number}
  */
 var minGroups = function(intervals) {
-    intervals.sort((a, b) => a[0] - b[0]);
-    let pq = new MinPriorityQueue();
-    
-    for (let [start, end] of intervals) {
-        if (!pq.isEmpty() && pq.front().element < start) {
-            pq.dequeue();
-        }
-        pq.enqueue(end);
-    }
-    
-    return pq.size();
+  let n = intervals.length;
+  let a = new Uint32Array(n << 1);
+  for (let i = 0; i < n; ++i) {
+    a[i << 1] = intervals[i][0] << 1;
+    a[(i << 1) | 1] = (intervals[i][1] << 1) | 1;
+  }
+  let res = 1;
+  let cur = 0;
+  n <<= 1;
+  a.sort((a,b) => a - b);
+  for (let i = 0; i < n; ++i) if (a[i] & 1) --cur
+  else {
+    ++cur;
+    res = Math.max(res, cur);
+  }
+
+  return res;
 };
