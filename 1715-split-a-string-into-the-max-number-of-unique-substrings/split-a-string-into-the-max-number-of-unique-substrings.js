4c8cf88bf77716455ce365e6f71f5257bcda6f82
Thomas Ashraf <66570810+thomas-x-69@users.noreply.github.com>
2024-10-21 05:21:17 +0300

    Time: 39 ms (96.30%) | Memory: 54.7 MB (40.74%) - LeetSync

--- 1715-split-a-string-into-the-max-number-of-unique-substrings/split-a-string-into-the-max-number-of-unique-substrings.js
+++ 1715-split-a-string-into-the-max-number-of-unique-substrings/split-a-string-into-the-max-number-of-unique-substrings.js
@@ -3,24 +3,30 @@
  * @return {number}
  */
 var maxUniqueSplit = function(s) {
-      const ans = { val: 0 };
-  const set = new Set([]);
-  helper(ans, set, s, 0);
+    const set = new Set();
 
-  return ans.val;
-};
+    const traverse = (s, start, set) => {
+        if (start === s.length) {
+            return 0;
+        }
 
-function helper(ans, set, s, idx) {
-  if (idx >= s.length) {
-    ans.val = Math.max(ans.val, set.size);
-    return;
-  }
+        let max = 0;
 
-  for (let i = idx; i < s.length; i++) {
-    const substring = s.slice(idx, i + 1);
-    if (set.has(substring)) continue;
-    set.add(substring);
-    helper(ans, set, s, i + 1);
-    set.delete(substring);
-  }
+        for (let i = start + 1; i <= s.length; i++) {
+            let substr = s.substring(start, i);
+            if (set.has(substr)) {
+                continue;
+            }
+
+            set.add(substr);
+
+            max = Math.max(max, 1 + traverse(s, i, set))
+
+            set.delete(substr);
+        }
+
+        return max;
+    }
+
+    return traverse(s, 0, set)
 };
