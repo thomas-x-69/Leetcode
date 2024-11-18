baabe6f72abed5a793403875bad19573a122e601
Thomas Ashraf <66570810+thomas-x-69@users.noreply.github.com>
2024-11-19 01:21:00 +0200

    Time: 161 ms (43.18%) | Memory: 68 MB (65.61%) - LeetSync

---
+++ 892-shortest-subarray-with-sum-at-least-k/shortest-subarray-with-sum-at-least-k.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var shortestSubarray = function(nums, k) {
+    const n = nums.length;
+    const prefix = new Array(n + 1).fill(0);
+
+    for (let i = 0; i < n; i++) {
+        prefix[i + 1] = prefix[i] + nums[i];
+    }
+
+    const deque = [];
+    let minLength = Infinity;
+
+    for (let i = 0; i <= n; i++) {
+        while (deque.length > 0 && prefix[i] - prefix[deque[0]] >= k) {
+            minLength = Math.min(minLength, i - deque.shift());
+        }
+
+        while (deque.length > 0 && prefix[i] <= prefix[deque[deque.length - 1]]) {
+            deque.pop();
+        }
+
+        deque.push(i);
+    }
+
+    return minLength === Infinity ? -1 : minLength;
+};
