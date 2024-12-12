6f00995c5984bfa5e3d020e9a22feb1920a0ffe0
Thomas Ashraf <66570810+thomas-x-69@users.noreply.github.com>
2024-12-12 23:06:16 +0200

    Time: 112 ms (30.00%) | Memory: 56.2 MB (7.14%) - LeetSync

---
+++ 2692-take-gifts-from-the-richest-pile/take-gifts-from-the-richest-pile.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} gifts
+ * @param {number} k
+ * @return {number}
+ */
+var pickGifts = function(gifts, k) {
+    gifts.sort((a, b) => a - b);
+
+    for (let i = 0; i < k; i++) {
+        gifts[gifts.length - 1] = Math.floor(Math.sqrt(gifts[gifts.length - 1]));
+        gifts.sort((a, b) => a - b);
+    }
+
+    return gifts.reduce((sum, gift) => sum + gift, 0);
+};
