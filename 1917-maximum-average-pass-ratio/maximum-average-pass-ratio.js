73b1e3b712a79133eddeb7187c2498974389ce73
Thomas Ashraf <66570810+thomas-x-69@users.noreply.github.com>
2024-12-15 03:13:16 +0200

    Time: 1146 ms (50.00%) | Memory: 95.2 MB (33.33%) - LeetSync

---
+++ 1917-maximum-average-pass-ratio/maximum-average-pass-ratio.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[][]} classes
+ * @param {number} extraStudents
+ * @return {number}
+ */
+var maxAverageRatio = function (classes, extraStudents) {
+    let heap = new MaxPriorityQueue({ priority: x => x[2] });
+
+    for (let [pass, total] of classes) {
+        let before = pass / total;
+        let after = (pass + 1) / (total + 1);
+        heap.enqueue([pass, total, after - before]);
+    }
+
+    while (extraStudents--) {
+        let [pass, total] = heap.dequeue().element;
+        pass++;
+        total++;
+        let before = pass / total;
+        let after = (pass + 1) / (total + 1);
+        heap.enqueue([pass, total, after - before]);
+    }
+
+    let sum = 0;
+
+    while (!heap.isEmpty()) {
+        let [pass, total] = heap.dequeue().element;
+        sum += pass / total;
+    }
+    return sum / classes.length;
+};
