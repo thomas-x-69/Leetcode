83bbf7373ba5563b7cf4cbfa7de9169c05b5c926
Thomas Ashraf <66570810+thomas-x-69@users.noreply.github.com>
2025-03-09 03:04:49 +0200

    Time: 15 ms (14.19%) | Memory: 58.3 MB (10.00%) - LeetSync

--- 1184-car-pooling/car-pooling.js
+++ 1184-car-pooling/car-pooling.js
@@ -10,8 +10,8 @@ var carPooling = function(trips, capacity) {
     {
      let [j,first, last] = trips[i];
      while (first < last) {
-        if (!arr[first ]){arr[first] = 0}
-            arr[first ] += j;
+        if (!arr[first]){arr[first] = 0}
+            arr[first] += j;
             first++;
         }
     }
