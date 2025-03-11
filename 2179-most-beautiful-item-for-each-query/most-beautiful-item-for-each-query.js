c8336f21f1c3c1dbcde0b989c15346acf550741f
Thomas Ashraf <66570810+thomas-x-69@users.noreply.github.com>
2025-03-11 02:34:48 +0200

    Time: 117 ms (40.00%) | Memory: 94.2 MB (20.00%) - LeetSync

--- 2179-most-beautiful-item-for-each-query/most-beautiful-item-for-each-query.js
+++ 2179-most-beautiful-item-for-each-query/most-beautiful-item-for-each-query.js
@@ -3,34 +3,20 @@
  * @param {number[]} queries
  * @return {number[]}
  */
-var maximumBeauty = function (items, queries) {
-    const maxI = Number.MAX_SAFE_INTEGER;
-    let res = [[0, 0, maxI]]
+var maximumBeauty = function(items, queries) {
+     items.sort((a, b) => a[0] - b[0]);
+    let queriesWithIndex = queries.map((q, i) => [q, i]).sort((a, b) => a[0] - b[0]);
+    
+    let res = new Array(queries.length).fill(0);
+    let maxBea = 0;
+    let j = 0;
 
-    items.sort((a, b) => a[0] - b[0])
-
-    for (let item of items) {
-        let price = item[0]
-        let beauty = item[1]
-        let lastBracket = res.at(-1)
-
-        if (beauty > lastBracket[1]) {
-            res[res.length - 1][2] = price
-            res.push([price, beauty, maxI])
+      for (let [query, index] of queriesWithIndex) {
+        while (j < items.length && items[j][0] <= query) {
+            maxBea = Math.max(maxBea, items[j][1]);
+            j++;
         }
+        res[index] = maxBea;
     }
-
-    let ans = []
-
-    for (let x of queries) {
-        for (let i = res.length - 1; i > -1; i--) {
-            if (res[i][0] <= x) {
-                ans.push(res[i][1])
-                break
-            }
-        }
-    }
-
-    return ans
-
+    return res;
 };
