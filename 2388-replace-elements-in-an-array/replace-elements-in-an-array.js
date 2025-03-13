1d43f4feb8646f4ababdb539d812d4cb3fd1b473
Thomas Ashraf <66570810+thomas-x-69@users.noreply.github.com>
2025-03-13 22:50:01 +0200

    Time: 70 ms (28.57%) | Memory: 99.4 MB (42.86%) - LeetSync

---
+++ 2388-replace-elements-in-an-array/replace-elements-in-an-array.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[]} nums
+ * @param {number[][]} operations
+ * @return {number[]}
+ */
+var arrayChange = function (nums, operations) {
+    let map = new Map()
+
+    for (let i = 0; i < nums.length; i++) {
+        map.set(nums[i], i);
+    }
+    console.log(map)
+
+
+    for (let op of operations) {
+        let key = op[0];
+        let value = op[1];
+        if (map.has(key)) {
+            let idx = map.get(key);
+            map.set(value, idx);
+            map.delete(key);
+        }
+    }
+    for (let [key, idx] of map) {
+        nums[idx] = key;
+    }
+
+    return nums;
+
+
+};
