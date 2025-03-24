c09b4b8831d50bba857a6064ac5795fa3523a052
Thomas Ashraf <66570810+thomas-x-69@users.noreply.github.com>
2025-03-25 01:08:10 +0200

    Time: 5 ms (38.77%) | Memory: 58.7 MB (32.35%) - LeetSync

---
+++ 57-insert-interval/insert-interval.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[][]} intervals
+ * @param {number[]} newInterval
+ * @return {number[][]}
+ */
+var insert = function(intervals, newInterval) {
+    
+     intervals.push(newInterval)
+     intervals.sort((a, b) => a[0] - b[0])
+    const result = [intervals[0]];
+
+    for (let i = 1; i < intervals.length; i++) {
+        const current = intervals[i];
+        const lastMerged = result[result.length - 1];
+
+        if (current[0] <= lastMerged[1]) {
+            lastMerged[1] = Math.max(lastMerged[1], current[1]);
+        } else {
+            result.push(current);
+        }
+    }
+    return result
+};
