1862a8294947b742af6fbd45de9c28054bcb2e80
Thomas Ashraf <66570810+thomas-x-69@users.noreply.github.com>
2025-05-30 22:23:31 +0300

    Time: 1 ms (84.93%) | Memory: 55.1 MB (59.24%) - LeetSync

---
+++ 797-rabbits-in-forest/rabbits-in-forest.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} answers
+ * @return {number}
+ */
+var numRabbits = function (answers) {
+    const freq = {};
+    for (const a of answers) {
+        freq[a] = (freq[a] || 0) + 1;
+    }
+
+    let count = 0;
+    for (const k in freq) {
+        const key = parseInt(k);
+        const v = freq[k];
+        const groupSize = key + 1;
+        const groups = Math.ceil(v / groupSize);
+        count += groups * groupSize;
+    }
+
+    return count;
+};
