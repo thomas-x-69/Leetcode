b232d04e53967707ff83003df997f7a1bb20356e
Thomas Ashraf <66570810+thomas-x-69@users.noreply.github.com>
2025-06-04 01:51:43 +0300

    Time: 139 ms (5.71%) | Memory: 71.3 MB (7.14%) - LeetSync

---
+++ 991-array-of-doubled-pairs/array-of-doubled-pairs.js
@@ -0,0 +1,37 @@
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var canReorderDoubled = function (arr) {
+    arr = arr.sort((a, b) => a - b)
+    let map = {}
+    for (e of arr) {
+        map[e] ? map[e] += 1 : map[e] = 1
+    }
+    for (e of arr) {
+        if (e == 0) {
+
+            if(map[e] >= 2)
+            {map[e]--
+            map[e]--
+            }
+            else continue
+        }
+      
+        
+         else if ((map[e] && map[e * 2])) {
+
+
+            map[e]--
+            map[e * 2]--
+
+        } else if (map[e] && map[e / 2]) {
+            map[e]--
+            map[e / 2]--
+
+        }
+
+    }
+    if (Object.values(map).reduce((a, b) => a + b) > 0) return false
+    else return true
+};
