c2027cd84838536fd4d170d89bca0e48ed5c8289
Thomas Ashraf <66570810+thomas-x-69@users.noreply.github.com>
2025-06-05 19:55:29 +0300

    Time: 46 ms (16.67%) | Memory: 75.1 MB (16.67%) - LeetSync

--- 3228-maximum-size-of-a-set-after-removals/maximum-size-of-a-set-after-removals.js
+++ 3228-maximum-size-of-a-set-after-removals/maximum-size-of-a-set-after-removals.js
@@ -4,18 +4,18 @@
  * @return {number}
  */
 var maximumSetSize = function (nums1, nums2) {
-   const n = nums1.length;
-    const keep = n / 2; 
-    
+    const n = nums1.length;
+    const keep = n / 2;
+
     const unique1 = new Set(nums1);
     const unique2 = new Set(nums2);
-    
+
     const maxFrom1 = Math.min(keep, unique1.size);
     const maxFrom2 = Math.min(keep, unique2.size);
-    
+
     const total = maxFrom1 + maxFrom2;
-    
+
     const totalUnique = new Set([...unique1, ...unique2]).size;
-    
+
     return Math.min(total, totalUnique);
 };
