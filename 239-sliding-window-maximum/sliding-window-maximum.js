adc08aa462d2cd8877e8b366f217d063df497972
Thomas Ashraf <66570810+thomas-x-69@users.noreply.github.com>
2025-06-14 05:31:32 +0300

    Time: 420 ms (58.57%) | Memory: 85.3 MB (80.94%) - LeetSync

---
+++ 239-sliding-window-maximum/sliding-window-maximum.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var maxSlidingWindow = function (nums, k) {
+   let res = [];
+    let arr = [];
+
+    for (let i = 0; i < nums.length; i++) {
+
+        while (arr.length && arr[arr.length - 1] < nums[i]) {
+            arr.pop();
+        }
+        arr.push(nums[i]);
+
+        if (i >= k && nums[i - k] === arr[0]) {
+            arr.shift();
+        }
+
+        if (i >= k - 1) {
+            res.push(arr[0]);
+        }
+    }
+
+    return res;    
+};
