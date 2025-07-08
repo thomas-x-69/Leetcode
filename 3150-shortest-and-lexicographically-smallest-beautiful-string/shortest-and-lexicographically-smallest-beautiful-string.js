3ff34f7c295ed65c20671732bf0997eae00f0ced
Thomas Ashraf <66570810+thomas-x-69@users.noreply.github.com>
2025-07-08 20:55:06 +0300

    Time: 1 ms (82.61%) | Memory: 55.6 MB (82.61%) - LeetSync

--- 3150-shortest-and-lexicographically-smallest-beautiful-string/shortest-and-lexicographically-smallest-beautiful-string.js
+++ 3150-shortest-and-lexicographically-smallest-beautiful-string/shortest-and-lexicographically-smallest-beautiful-string.js
@@ -7,18 +7,18 @@ var shortestBeautifulSubstring = function (s, k) {
   let l = 0, r = 0, ones = 0, res = '';
     
     while (r < s.length) {
-        if (s[r] === '1') ones++;
+        if (s[r] == 1) ones++;
         r++;
         
-        while (ones > k || (ones === k && s[l] === '0')) {
-            if (s[l] === '1') ones--;
+        while (ones > k || (ones == k && s[l] == 0)) {
+            if (s[l] == 1) ones--;
             l++;
         }
         
-        if (ones === k) {
+        if (ones == k) {
             let current = s.slice(l, r);
             if (!res || current.length < res.length || 
-                (current.length === res.length && current < res)) {
+                (current.length == res.length && current < res)) {
                 res = current;
             }
         }
