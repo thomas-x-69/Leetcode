acad67bb453f6834be25c8725bd73da51252a5d1
Thomas Ashraf <66570810+thomas-x-69@users.noreply.github.com>
2026-01-20 20:05:10 +0200

    Time: 69 ms (9.56%) | Memory: 79.3 MB (84.73%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.js
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.js
@@ -3,14 +3,20 @@
  * @return {number}
  */
 var longestConsecutive = function (nums) {
-    nums = [...new Set(nums.sort((a, b) => a - b))]
-    let max = 1
-    let res = 1
+    if (nums.length == 0) return 0
+    nums = [...new Set(nums.sort((a, b) => a - b))];
+    console.log(nums)
+    let temp = 0
+    let res = 0
     for (i = 1; i < nums.length; i++) {
-        if (nums[i] - nums[i - 1] == 1) { max++ }
-        else { max = 1 }
+        if (nums[i] - nums[i - 1] > 1) {
+            temp = 0
+        }
+         if (nums[i] - nums[i - 1] == 1) {
+        temp++
+        }
+        temp > res ? res = temp : null
 
-        res = Math.max(res, max)
     }
-    return nums.length>0?res:0
+    return res + 1
 };
