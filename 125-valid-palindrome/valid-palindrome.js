eb6126d755d65040f894dc989c2bcbf64594c160
Thomas Ashraf <66570810+thomas-x-69@users.noreply.github.com>
2026-01-22 01:58:49 +0200

    Time: 2 ms (92.79%) | Memory: 57.7 MB (67.74%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.js
+++ 125-valid-palindrome/valid-palindrome.js
@@ -4,8 +4,7 @@
  */
 var isPalindrome = function (s) {
     s = s.toLowerCase().replace(/[^a-z0-9]/g, '');
-    let l = 0;
-    let r = s.length - 1;
+    let l = 0, r = s.length - 1;
     while (l < r) {
         if (s[l] !== s[r]) return false
         l++
