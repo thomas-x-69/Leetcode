f9988c6cb2eafb5f9ca134f1dc9c38c961035ab1
Thomas Ashraf <66570810+thomas-x-69@users.noreply.github.com>
2026-01-22 02:31:40 +0200

    Time: 1 ms (82.47%) | Memory: 56.6 MB (28.91%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.js
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.js
@@ -3,16 +3,31 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
-    // let map = {}
-    for(i=0; i<nums.length; i++)
-    {
-        let complement = target - nums[i]
-   if(nums.includes(complement,i+1))
-   {
-    return [i+1,nums.indexOf(complement,i+1)+1]
-   }
+var twoSum = function (nums, target) {
+    //!----- solution 1: not optimal -------!
+
+    // for (i = 0; i < nums.length; i++) {
+    //     let complement = target - nums[i]
+    //     if (nums.includes(complement, i + 1)) {
+    //         return [i + 1, nums.indexOf(complement, i + 1) + 1]
+    //     }
+    // }
+
+    //!----- solution 2: optimal -------!
+
+    let l = 0;
+    let r = nums.length-1;
+    while (l<r) {
+        if(nums[l]+nums[r]==target)
+        {
+            return [l+1,r+1]
+        }else if(nums[l]+nums[r]>target)
+        {
+            r--
+        }else if (nums[l]+nums[r]<target)
+        {
+            l++
+        }
     }
-    
-    
+
 };
