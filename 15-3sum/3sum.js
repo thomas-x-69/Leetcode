3a37bcd6e2d941b97c7a1f9282ade36c8b6bac13
Thomas Ashraf <66570810+thomas-x-69@users.noreply.github.com>
2026-01-30 07:58:26 +0200

    Time: 40 ms (34.81%) | Memory: 72.5 MB (59.69%) - LeetSync

--- 15-3sum/3sum.js
+++ 15-3sum/3sum.js
@@ -5,15 +5,12 @@
 var threeSum = function (nums) {
     nums.sort((a, b) => a - b)
     let res = []
-
-    for (let i = 0; i < nums.length - 2; i++) {
+    for (i = 0; i < nums.length - 2; i++) {
         if (i > 0 && nums[i] === nums[i - 1]) continue
 
-        let l = i + 1, r = nums.length - 1
-
+        let l = i + 1, r = nums.length - 1;
         while (l < r) {
             let sum = nums[i] + nums[l] + nums[r]
-
             if (sum === 0) {
                 res.push([nums[i], nums[l], nums[r]])
 
@@ -28,13 +25,9 @@ var threeSum = function (nums) {
             } else {
                 r--
             }
+
         }
+
     }
-
     return res
-
-
 };
-process.on("exit", () => {
-    require("fs").writeFileSync("display_runtime.txt", "0");
-});
