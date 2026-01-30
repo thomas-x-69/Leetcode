56911454819fc6ece1f7fca5238bd393d9d99597
Thomas Ashraf <66570810+thomas-x-69@users.noreply.github.com>
2026-01-30 07:59:50 +0200

    Time: 36 ms (59.93%) | Memory: 71.2 MB (94.12%) - LeetSync

--- 15-3sum/3sum.js
+++ 15-3sum/3sum.js
@@ -15,7 +15,7 @@ var threeSum = function (nums) {
                 res.push([nums[i], nums[l], nums[r]])
 
                 l++
-                r--
+                // r--
 
                 while (l < r && nums[l] === nums[l - 1]) l++
                 while (l < r && nums[r] === nums[r + 1]) r--
