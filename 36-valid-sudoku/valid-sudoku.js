1fac26620d7a6b47f8c1026617772cceac12b809
Thomas Ashraf <66570810+thomas-x-69@users.noreply.github.com>
2026-01-31 04:48:36 +0200

    Time: 3 ms (86.72%) | Memory: 56.8 MB (65.53%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.js
+++ 36-valid-sudoku/valid-sudoku.js
@@ -3,32 +3,38 @@
  * @return {boolean}
  */
 var isValidSudoku = function (board) {
+    // check the big box 
+    // check the row and col in smaller loop 
+    // if contains duplicate then return false
+    // else return true
+
     for (i = 0; i < 9; i++) {
-        let row = new Set(),
-            col = new Set(),
-            box = new Set();
+
+        let Inrow = new Set()
+        let Incol = new Set()
+        let Inbox = new Set()
         for (j = 0; j < 9; j++) {
-            R = board[i][j]
-            C = board[j][i]
-            B = board[3 * Math.floor(i / 3) + Math.floor(j / 3)][3 * (i % 3) + (j % 3)];
+            let Row = board[i][j]
+            let Col = board[j][i]
+            let Box = board[3*Math.floor(i/3)+Math.floor(j/3)][3*(i%3)+(j%3)]
 
-            if (R!=".")
-            {
-                if(row.has(R))return false
-                row.add(R)
+        if (Row != ".") {
+                if (Inrow.has(Row)) return false
+                Inrow.add(Row)
             }
-            if (C!=".")
-            {
-                  if(col.has(C))return false
-                col.add(C)
+
+            if (Col != ".") {
+                if (Incol.has(Col)) return false
+                Incol.add(Col)
             }
-            if (B!=".")
-            {
-                  if(box.has(B))return false
-                box.add(B)
+
+            if (Box != ".") {
+                if (Inbox.has(Box)) return false
+                Inbox.add(Box)
             }
+
+
         }
-
     }
     return true
 };
