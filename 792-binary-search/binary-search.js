dca72bc2ab05e4ef6b571f46b9d8821ae0111dc4
Thomas Ashraf <66570810+thomas-x-69@users.noreply.github.com>
2026-02-02 23:33:27 +0200

    Time: 1 ms (9.94%) | Memory: 58.6 MB (10.75%) - LeetSync

---
+++ 792-binary-search/binary-search.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+
+    let left = 0
+    let right = nums.length-1
+
+    while(left<=right)
+    {
+        let mid = Math.floor(right-left/2)
+        if(target>nums[mid])
+        {
+            left = mid+1
+        }else if(target<nums[mid])
+        {
+            right = mid-1
+
+        }else if(target==nums[mid])
+        {
+            return mid
+        } 
+
+    }
+    return -1
+
+    
+};
