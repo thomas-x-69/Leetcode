b076fdfc25f238cf1757c5b80a86de953f041244
Thomas Ashraf <66570810+thomas-x-69@users.noreply.github.com>
2026-02-03 07:41:25 +0200

    Time: 17 ms (51.47%) | Memory: 58.4 MB (57.50%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.js
+++ 567-permutation-in-string/permutation-in-string.js
@@ -3,18 +3,16 @@
  * @param {string} s2
  * @return {boolean}
  */
-var checkInclusion = function(s1 , s2) {
-  if (s1.length > s2.length) return false;
-
-    let s1Count = Array(26).fill(0);
+var checkInclusion = function(s1, s2) {
+    
+     let s1Count = Array(26).fill(0);
     let s2Count = Array(26).fill(0);
 
     for (let i = 0; i < s1.length; i++) {
         s1Count[s1.charCodeAt(i) - 97]++;
         s2Count[s2.charCodeAt(i) - 97]++;
     }    
-
-    for(let i=0;i<s2.length ;i++)
+     for(let i=0;i<s2.length ;i++)
     {
         if(isMatch(s1Count,s2Count))return true
      s2Count[s2.charCodeAt(i) - 97]--;
@@ -22,6 +20,7 @@ var checkInclusion = function(s1 , s2) {
     }
     return isMatch(s1Count, s2Count);
 };
+
 
 function isMatch(s1Count, s2Count)
 {
