93cae2e5f5976c3e13f47c203123395bd1727714
Thomas Ashraf <66570810+thomas-x-69@users.noreply.github.com>
2026-02-03 05:35:09 +0200

    Time: 12 ms (34.37%) | Memory: 75.9 MB (5.48%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.js
+++ 238-product-of-array-except-self/product-of-array-except-self.js
@@ -5,28 +5,26 @@
 var productExceptSelf = function (nums) {
     let prefix = [nums[0]]
     let suffix = [nums[nums.length - 1]]
+    let res = []
     for (let i = 1, j = nums.length - 2; i < nums.length; i++, j--) {
         prefix.push(prefix[i - 1] * nums[i])
         suffix.push(nums[j] * suffix[i - 1])
 
     }
     suffix.reverse()
-  
-    let res = []
     for(i=0;i<nums.length;i++)
     {
-        let prod = 1
+        let num = 1
         if(i>0)
         {
-            prod = prod*prefix[i-1]
+            num*=prefix[i-1]
         }
         if(i<nums.length-1)
         {
-            prod = prod*suffix[i+1]
+            num*=suffix[i+1]
+
         }
-        res.push(prod)
+        res.push(num)
     }
     return res
-
-
 };
