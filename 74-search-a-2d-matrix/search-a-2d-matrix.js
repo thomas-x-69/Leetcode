8d814bb7316f3e69dd8f62fb6e71179fdfbb4a04
Thomas Ashraf <66570810+thomas-x-69@users.noreply.github.com>
2026-02-04 10:34:40 +0200

    Time: 0 ms (100.00%) | Memory: 54 MB (61.53%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.js
+++ 74-search-a-2d-matrix/search-a-2d-matrix.js
@@ -4,24 +4,22 @@
  * @return {boolean}
  */
 var searchMatrix = function (matrix, target) {
-    matrix = matrix.flat()
-
-    let left = 0
-    let right = matrix.length - 1
-
-    while (left <= right) {
-        const mid = Math.floor((left + right) / 2);
-        if (matrix[mid] === target) {
-            return true;
-        }
-        else if (matrix[mid] < target) {
-            left = mid + 1;
-        }
-        else {
-            right = mid - 1;
+    for (let i = 0; i < matrix.length; i++) {
+        let arr = matrix[i];
+        let left = 0
+        let right = arr.length - 1
+        let mid = Math.floor(arr.length / 2)-1
+        while (left <= right) {
+            if (arr[mid] > target) {
+                right = mid-1
+            } else if (arr[mid] < target) {
+                 left = mid+1
+            } else if (arr[mid] == target)  {
+                return true
+            }
+            mid = Math.floor((right+left) / 2)
+            
         }
     }
-
     return false
-
 };
