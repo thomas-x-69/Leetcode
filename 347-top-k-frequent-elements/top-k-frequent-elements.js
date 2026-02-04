666ec5b88847666cdc6427eaabeea210578d40aa
Thomas Ashraf <66570810+thomas-x-69@users.noreply.github.com>
2026-02-04 08:21:33 +0200

    Time: 11 ms (69.45%) | Memory: 59.4 MB (69.88%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.js
+++ 347-top-k-frequent-elements/top-k-frequent-elements.js
@@ -3,15 +3,17 @@
  * @param {number} k
  * @return {number[]}
  */
-var topKFrequent = function (nums, k) {
-    const hash = new Map();
-
-    for (let num of nums) {
-        hash.set(num, (hash.get(num) || 0) + 1)
+var topKFrequent = function(nums, k) {
+    // freq
+    // sort
+    // to array + slice "k" elements
+    // 
+    let freq = {}
+    for(i=0;i<nums.length;i++)
+    {
+    freq[nums[i]]?freq[nums[i]]++:freq[nums[i]]=1
     }
-
-    let sorted = new Map([...hash.entries()].sort(([, a], [, b]) => a - b))
-
-    return Array.from(sorted.keys()).slice(-k)
-
+    
+    return Object.entries(freq).sort((a,b)=>b[1]-a[1]).map((a)=>Number(a[0])).slice(0,k)
+    
 };
