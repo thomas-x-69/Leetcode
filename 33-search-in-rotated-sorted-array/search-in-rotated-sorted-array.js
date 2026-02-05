e21b07eb0c2a9336473a0a1dd006c10161a1dc78
Thomas Ashraf <66570810+thomas-x-69@users.noreply.github.com>
2026-02-05 13:38:53 +0200

    Time: 0 ms (100.00%) | Memory: 54.3 MB (31.33%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.js
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.js
@@ -3,23 +3,36 @@
  * @param {number} target
  * @return {number}
  */
-var search = function(nums, target) {
-     let left=0, right = nums.length - 1
+var search = function (nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left < right) {
+        let mid = Math.floor((left + right) / 2);
+
+        if (nums[mid] > nums[right]) {
+            left = mid + 1;
+        } else {
+            right = mid;
+        }
+    }
+    let pivot = left
+    left = 0;
+    right = nums.length - 1;
+
+    if (target >= nums[pivot] && target <= nums[right]) {
+        left = pivot;
+    } else {
+        right = pivot - 1;
+    }
 
     while (left <= right) {
-        let mid = Math.floor(left + (right - left) / 2)
-        if (nums[mid] == target)
-            return mid
-        if (nums[left] <= nums[mid]) {
-            if (nums[left] <= target && target < nums[mid])
-                right = mid - 1
-            else
-                left = mid + 1
-        }
-        else if (nums[mid] < target && target <= nums[right])
-            left = mid + 1
-        else
-            right = mid - 1
+        let mid = Math.floor((left + right) / 2);
+
+        if (nums[mid] === target) return mid;
+        else if (nums[mid] < target) left = mid + 1;
+        else right = mid - 1;
     }
-    return -1
+
+    return -1;
 };
