f757b9766548bcfbb6a5a646dbc64021070de964
Thomas Ashraf <66570810+thomas-x-69@users.noreply.github.com>
2026-02-13 22:35:46 +0200

    Time: 31 ms (11.12%) | Memory: 63.4 MB (10.68%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.js
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.js
@@ -5,17 +5,12 @@
 var evalRPN = function (tokens) {
     let stack = []
     for (let token of tokens) {
-
         if (!isNaN(+token)) {
             stack.push(Number(token));
-
         } else {
             let b = stack.pop();
             let a = stack.pop();
-            if (token == "+") stack.push(a + b);
-            else if (token == "-") stack.push(a - b);
-            else if (token == "*") stack.push(a * b);
-            else stack.push(Math.trunc(a / b));
+            stack.push(Math.trunc(eval((`${a} ${token} ${b}`))))
         }
     }
     return stack[0]
