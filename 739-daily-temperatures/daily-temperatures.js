1163e22e715da65b3669ffa3d7f2d6101eef84d0
Thomas Ashraf <66570810+thomas-x-69@users.noreply.github.com>
2026-02-13 21:52:56 +0200

    Time: 12 ms (95.54%) | Memory: 80.9 MB (24.55%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.js
+++ 739-daily-temperatures/daily-temperatures.js
@@ -1,13 +1,13 @@
 /**
- * @param {number[]} temperatures
+ * @param {number[]} temps
  * @return {number[]}
  */
-var dailyTemperatures = function (temperatures) {
-    const n = temperatures.length;
+var dailyTemperatures = function (temps) {
+    const n = temps.length;
     const result = new Array(n).fill(0);
     const stack = [];
     for (let i = n - 1; i >= 0; i--) {
-        while (stack.length > 0 && temperatures[i] >= temperatures[stack.at(-1)]) {
+        while (stack.length > 0 && temps[i] >= temps[stack.at(-1)]) {
             stack.pop();
         }
         if (stack.length > 0) {
