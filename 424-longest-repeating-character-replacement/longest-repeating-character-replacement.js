456ac33810cdea06dd994f17935504723a15d167
Thomas Ashraf <66570810+thomas-x-69@users.noreply.github.com>
2026-02-14 12:46:59 +0200

    Time: 17 ms (72.20%) | Memory: 56.9 MB (56.25%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.js
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.js
@@ -3,19 +3,18 @@
  * @param {number} k
  * @return {number}
  */
-var characterReplacement = function (s, k) {
-    let left = 0, maxCount = 0;
+var characterReplacement = function(s, k) {
+  let left = 0, maxCount = 0;
     const freq = {};
 
     for (let right = 0; right < s.length; right++) {
         freq[s[right]] = (freq[s[right]] || 0) + 1;
         maxCount = Math.max(maxCount, freq[s[right]]);
-
-        if (right - left + 1 - maxCount > k) {
+         if (right - left + 1 - maxCount > k) {
             freq[s[left]]--;
             left++;
         }
-    }
 
-    return s.length - left;
+    }
+     return s.length - left;
 };
